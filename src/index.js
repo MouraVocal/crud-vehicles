f5d7a9cfa5db765f17b4a98c48ab2b36a75435da
MouraVocal <diego.moura3@gmail.com>
2021-08-26 19:22:07 -0300

    added nodemon, uuid, GET and POST routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
-const server = express();
+const app = express();
 
-server.listen(3000);
+app.listen(3000);
+app.use(express.json());
+
+const vehicles = [];
+
+// Get vehicles list
+app.get('/vehicles', (req, res) => {
+  res.json(vehicles);
+});
+
+// Post new vehicles
+app.post('/vehicles', (req, res) => {
+  const {
+    placa, chassi, renavam, modelo, marca, ano,
+  } = req.body;
+  const vehicle = {
+    id: uuidv4(), placa, chassi, renavam, modelo, marca, ano,
+  };
+  vehicles.push(vehicle);
+  return res.json(vehicle);
+});
