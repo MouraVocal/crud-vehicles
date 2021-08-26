30af3ea6667162b50bc1c6b317c542ee7dcbde6d
MouraVocal <diego.moura3@gmail.com>
2021-08-26 20:27:31 -0300

    added PUT and DELETE routes

--- src/index.js
+++ src/index.js
@@ -24,3 +24,37 @@ app.post('/vehicles', (req, res) => {
   vehicles.push(vehicle);
   return res.json(vehicle);
 });
+
+// Update vehicle
+app.put('/vehicles/:id', (req, res) => {
+  const { id } = req.params;
+
+  const {
+    placa, chassi, renavam, modelo, marca, ano,
+  } = req.body;
+
+  const vehicleIndex = vehicles.findIndex((vehicle) => vehicle.id === id);
+
+  if (vehicleIndex < 0) {
+    return res.status(400).json({ error: 'Vehicle Not Found!' });
+  }
+
+  const vehicle = {
+    id, placa, chassi, renavam, modelo, marca, ano,
+  };
+
+  vehicles[vehicleIndex] = vehicle;
+
+  return res.json(vehicle);
+});
+
+// Delete vehicle
+app.delete('/vehicles/:id', (req, res) => {
+  const { id } = req.params;
+
+  const vehicleIndex = vehicles.findIndex((vehicle) => vehicle.id === id);
+
+  vehicles.splice(vehicleIndex, 1);
+
+  return res.status(204).send();
+});
