4991f70716db506b8ada4373c2483ba34f4929f5
MouraVocal <diego.moura3@gmail.com>
2021-08-27 14:16:07 -0300

    added file save system

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
+/* eslint-disable no-console */
+/* eslint-disable no-shadow */
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 const app = express();
 
 app.listen(3000);
 app.use(express.json());
 
-const vehicles = [];
+// Checking if file exists
+let obj = {};
+obj.vehicles = [];
+const filePath = './file/vehicles.json';
+
+fs.access(filePath, fs.constants.F_OK, (err) => {
+  if (err) {
+    fs.writeFile(filePath, JSON.stringify(obj), (err) => {
+      console.log(err);
+    });
+  }
+  // eslint-disable-next-line consistent-return
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    obj = JSON.parse(data);
+  });
+});
+
+// Middleware for update file
+function updateFile(req, res, next) {
+  next();
+  fs.writeFile(filePath, JSON.stringify(obj), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+app.use(updateFile);
 
 // Get vehicles list
 app.get('/vehicles', (req, res) => {
-  res.json(vehicles);
+  res.json(obj.vehicles);
 });
 
 // Post new vehicles
@@ -21,7 +55,7 @@ app.post('/vehicles', (req, res) => {
   const vehicle = {
     id: uuidv4(), placa, chassi, renavam, modelo, marca, ano,
   };
-  vehicles.push(vehicle);
+  obj.vehicles.push(vehicle);
   return res.json(vehicle);
 });
 
@@ -33,7 +67,7 @@ app.put('/vehicles/:id', (req, res) => {
     placa, chassi, renavam, modelo, marca, ano,
   } = req.body;
 
-  const vehicleIndex = vehicles.findIndex((vehicle) => vehicle.id === id);
+  const vehicleIndex = obj.vehicles.findIndex((vehicle) => vehicle.id === id);
 
   if (vehicleIndex < 0) {
     return res.status(400).json({ error: 'Vehicle Not Found!' });
@@ -43,7 +77,7 @@ app.put('/vehicles/:id', (req, res) => {
     id, placa, chassi, renavam, modelo, marca, ano,
   };
 
-  vehicles[vehicleIndex] = vehicle;
+  obj.vehicles[vehicleIndex] = vehicle;
 
   return res.json(vehicle);
 });
@@ -52,9 +86,9 @@ app.put('/vehicles/:id', (req, res) => {
 app.delete('/vehicles/:id', (req, res) => {
   const { id } = req.params;
 
-  const vehicleIndex = vehicles.findIndex((vehicle) => vehicle.id === id);
+  const vehicleIndex = obj.vehicles.findIndex((vehicle) => vehicle.id === id);
 
-  vehicles.splice(vehicleIndex, 1);
+  obj.vehicles.splice(vehicleIndex, 1);
 
   return res.status(204).send();
 });
